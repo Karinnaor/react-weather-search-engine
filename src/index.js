5487b4bd44e8aac05989ae7efda8478b34c8433f
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2022-09-14 13:33:41 +0300

    added link to code source on Github

--- src/index.js
+++ src/index.js
@@ -2,15 +2,39 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import WeatherApp from "./WeatherApp";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <div className="weatherArea">
-      <h1>Weather Search Engine</h1>
-      <WeatherApp />
+    <div className="appBox">
+      <div className="weatherArea">
+        <h1>Weather Search Engine</h1>
+        <WeatherApp />
+      </div>
+      <p class="text-center mt-5">
+        ✌️ This project was coded by Karin Naor, and is{" "}
+        <a
+          href="https://github.com/Karinnaor/react-weather-search-engine"
+          target="_blank"
+          title="Karin's code on github"
+        >
+          open sourced
+        </a>{" "}
+        on
+        <span>
+          <FontAwesomeIcon icon="fa-brands fa-github" />
+        </span>
+        <a
+          href="https://www.github.com/karinnaor"
+          target="_blank"
+          title="Github profile"
+        >
+          Github
+        </a>
+      </p>
     </div>
   </StrictMode>
 );
